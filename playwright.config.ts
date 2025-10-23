355f48ebd939106014b0906fb47e3710d13c77d6
mohannadd mohammad <113944930+mohandalaidy171@users.noreply.github.com>
2025-10-23 16:11:42 +0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,26 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
   testMatch: /.*\.spec\.ts/,
   use: {
     headless: true,
-       screenshot: 'on',
-    video: 'on'
+    screenshot: 'on',
+    video: 'on',
   },
   reporter: [['list'], ['allure-playwright']],
+  projects: [
+    {
+      name: 'Chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'Firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'WebKit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
 });
